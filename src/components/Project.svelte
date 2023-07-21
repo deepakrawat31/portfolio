c8821b58002ef3854585a885ce35f838ecc99caf
Deepak Rawat <deepakrawat.mb@gmail.com>
2023-07-21 13:50:47 +0530

    Initial commit

---
+++ src/components/Project.svelte
@@ -0,0 +1,18 @@
+<script></script>
+
+<section id="projects" class="ring-2 ring-black flex flex-col gap-0.5">
+	<div
+		class="flex items-center justify-between p-2 bg-amber-400 ring-2 ring-black text-xl md:text-2xl uppercase font-bold"
+	>
+		<h2 class="select-none">Projects</h2>
+		<div class="flex items-center gap-2">
+			{#each { length: 3 } as _}
+				<span class="h-2 md:h-3 aspect-square rounded-full bg-slate-300 ring-2 ring-black" />
+			{/each}
+		</div>
+	</div>
+	<div class="p-8 bg-purple-400">
+		<h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</div>
+</section>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Project from '../components/Project.svelte';
+	import Header from '../components/Header.svelte';
+	import Hero from '../components/Hero.svelte';
+	import Contact from '../components/Contact.svelte';
+	import Footer from '../components/Footer.svelte';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import { quintInOut } from 'svelte/easing';
+
+	let animate = false;
+
+	onMount(() => {
+		animate = true;
+	});
+</script>
+
+<svelte:head>
+	<title>Deepak | Frontend Developer</title>
+</svelte:head>
+
+{#if animate}
+	<div
+		class="ring-2 ring-black flex flex-col gap-0.5"
+		in:fade={{
+			duration: 800,
+			delay: 100,
+			easing: quintInOut
+		}}
+	>
+		<Header />
+		<Hero />
+		<Project />
+		<Contact />
+		<Footer />
+	</div>
+{/if}
