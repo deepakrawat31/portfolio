7ce8a3b879e589e31913ede38c6905bd88e4907c
Deepak Rawat <deepakrawat.mb@gmail.com>
2024-01-27 21:52:52 +0530

    project section added

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,5 +1,15 @@
 <script>
-	import { IconBrandTailwind, IconBrandVue, IconDatabase } from '@tabler/icons-svelte';
+	import {
+		IconApi,
+		IconArrowRight,
+		IconBrandReact,
+		IconBrandTailwind,
+		IconBrandVue,
+		IconDatabase
+	} from '@tabler/icons-svelte';
+
+	import expenditure from '$lib/assets/expenditure.png';
+	import krypto from '$lib/assets/krypto.png';
 </script>
 
 <section id="projects" class="ring-2 ring-black flex flex-col gap-0.5">
@@ -13,7 +23,7 @@
 			{/each}
 		</div>
 	</div>
-	<div class="p-8 bg-purple-400 grid grid-cols-1 lg:grid-cols-2 gap-4">
+	<div class="p-4 md:p-8 bg-purple-400 grid grid-cols-1 lg:grid-cols-2 gap-4">
 		<div class="flex flex-col gap-0.5 ring-2 ring-black bg-slate-300">
 			<div
 				class="flex items-center justify-between p-2 bg-amber-400 ring-2 ring-black text-xl md:text-2xl uppercase font-bold"
@@ -21,19 +31,29 @@
 				<h2 class="select-none">expenditure</h2>
 				<span class="h-2 md:h-3 aspect-square rounded-full bg-slate-300 ring-2 ring-black" />
 			</div>
+			<div class="p-2">
+				<img src={expenditure} alt="expenditure" class="ring-2 ring-black" />
+			</div>
 			<div class="uppercase text-sm font-semibold p-2">
 				<p>expenditure is a place where you can keep track of your expenses/payouts over time.</p>
 			</div>
-			<div class="p-2 flex gap-4">
-				<span class="p-1 bg-amber-400 ring-2 ring-black"
-					><IconBrandVue size={32} stroke={1.5} /></span
-				>
-				<span class="p-1 bg-amber-400 ring-2 ring-black"
-					><IconBrandTailwind size={32} stroke={1.5} /></span
-				>
-				<span class="p-1 bg-amber-400 ring-2 ring-black"
-					><IconDatabase size={32} stroke={1.5} /></span
-				>
+			<div class="p-2 flex items-center justify-between">
+				<div class="flex gap-4">
+					<span class="p-1 bg-amber-400 ring-2 ring-black"
+						><IconBrandVue size={32} stroke={1.5} /></span
+					>
+					<span class="p-1 bg-amber-400 ring-2 ring-black"
+						><IconBrandTailwind size={32} stroke={1.5} /></span
+					>
+					<span class="p-1 bg-amber-400 ring-2 ring-black"
+						><IconDatabase size={32} stroke={1.5} /></span
+					>
+				</div>
+				<div class="p-1 bg-amber-400 ring-2 ring-black">
+					<a href="https://expenditure-nine.vercel.app">
+						<IconArrowRight size={32} stroke={1.5} />
+					</a>
+				</div>
 			</div>
 		</div>
 		<div class="flex flex-col gap-0.5 ring-2 ring-black bg-slate-300">
@@ -43,8 +63,27 @@
 				<h2 class="select-none">krypto</h2>
 				<span class="h-2 md:h-3 aspect-square rounded-full bg-slate-300 ring-2 ring-black" />
 			</div>
+			<div class="p-2">
+				<img src={krypto} alt="expenditure" class="ring-2 ring-black" />
+			</div>
 			<div class="uppercase text-sm font-semibold p-2">
 				<p>buying/exchanging crypto with secure and easy way of transaction.</p>
+			</div>
+			<div class="p-2 flex items-center justify-between">
+				<div class="flex gap-4">
+					<span class="p-1 bg-amber-400 ring-2 ring-black"
+						><IconBrandReact size={32} stroke={1.5} /></span
+					>
+					<span class="p-1 bg-amber-400 ring-2 ring-black"
+						><IconBrandTailwind size={32} stroke={1.5} /></span
+					>
+					<span class="p-1 bg-amber-400 ring-2 ring-black"><IconApi size={32} stroke={1.5} /></span>
+				</div>
+				<div class="p-1 bg-amber-400 ring-2 ring-black">
+					<a href="https://krypto-delta.vercel.app">
+						<IconArrowRight size={32} stroke={1.5} />
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
