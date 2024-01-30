1963c14d635ef874abdc7acf96ae05f54e5ded3d
Deepak Rawat <deepakrawat.mb@gmail.com>
2024-01-30 15:35:12 +0530

    resume update

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -43,7 +43,7 @@
 				<a href={`#${item.to}`} class="px-2 py-1 text-sm font-medium uppercase">{item.title}</a>
 			{/each}
 			<a
-				href="/files/deepakrawat_cv.pdf"
+				href="/files/deepak_frontend_cv.pdf"
 				download
 				class="px-2 py-1 text-sm font-medium uppercase bg-purple-400 ring-2 ring-black shadow-neubrut-4"
 				>Resume</a
@@ -97,7 +97,7 @@
 				>
 			{/each}
 			<a
-				href="/files/deepakrawat_cv.pdf"
+				href="/files/deepak_frontend_cv.pdf"
 				download
 				class="p-2 text-3xl font-medium uppercase bg-rose-400 ring-2 ring-black"
 				on:click={() => {
