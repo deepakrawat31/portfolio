056375e6d21b0582b12c811b15f33f9af19737f9
Deepak Rawat <deepakrawat.mb@gmail.com>
2025-09-21 23:14:54 +0530

    small update

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-	import IconPlusBold from 'phosphor-icons-svelte/IconPlusBold.svelte';
 </script>
 
-<header class="flex items-center justify-between gap-4 border-b border-b-surface-variant p-4">
-	<a href="/" class="flex flex-col font-cervanttis text-3xl font-bold">
-		<span>deepak</span>
-		<span>rawat</span>
-	</a>
-	<button
-		class="cursor-pointer rounded-full bg-surface-container p-3 text-on-surface-container transition-colors hover:bg-primary hover:text-on-primary"
+<header class="sticky top-0 z-50 border-b border-b-surface-variant bg-surface px-4">
+	<nav
+		class="flex w-full max-w-5xl items-center justify-between gap-4 place-self-center border-x border-x-surface-variant p-4 md:p-6 2xl:max-w-6xl"
 	>
-		<IconPlusBold class="size-6" />
-	</button>
+		<a
+			href="/"
+			class="flex flex-col gap-2 font-cervanttis text-2xl font-bold text-primary **:leading-none"
+		>
+			<span>deepak</span>
+			<span>rawat</span>
+		</a>
+	</nav>
 </header>
